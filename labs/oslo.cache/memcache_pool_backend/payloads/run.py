6e6f2b74823aeb8b02121690f5dc3980383fbbba
Hervé Beraud <hberaud@redhat.com>
2020-05-06 11:18:04 +0200

    improve payload

--- labs/oslo.cache/memcache_pool_backend/payloads/run.py
+++ labs/oslo.cache/memcache_pool_backend/payloads/run.py
@@ -1,9 +1,16 @@
 import os
+import threading
 from oslo_cache import core as cache
 from oslo_config import cfg
+import time
 
+PAYLOAD_BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 _CONFIG_FILES = [
-    os.path.join(os.path.dirname(os.path.abspath(__file__)), "app.conf"),
+    # Use the existing memcache_pool as the default backend
+    # Users could tests the new feature by consuming the related
+    # config to load the new pymemcache_pool backend.
+    # python payload/run.py --config-file payload/pymemcache_pool.conf
+    os.path.join(PAYLOAD_BASE_PATH, "memcache_pool-backend.conf"),
 ]
 
 test = 1
@@ -20,7 +27,7 @@ def my_key_generator(namespace, fn, **kw):
     fname = fn.__name__
     def generate_key(*arg):
         key = fname + "_".join(str(s) for s in arg)
-        print(key)
+        #print(key)
         return key
     return generate_key
 
@@ -34,23 +41,32 @@ print("Used backend: {}".format(backend))
 print("------------------------------------------------------")
 
 # Load config file here
-
 cache.configure_cache_region(CONF, example_cache_region)
 
 print("Cache configuration done")
 
+#region = cache.make_region()
+#region.set("herve", "boom")
+#print(region.get("herve"))
 
 @MEMOIZE
-def boom(x):
+def foo(x):
     print(x)
     return x
 
 @MEMOIZE
-def ohyeah(x):
+def bar(x):
     print(x)
     return x
 
-boom.set(14)
-ohyeah.set('foo')
-print(boom.get())
-print(dir(boom))
+
+
+for el in range(300):
+    print("~~~~~~~~~~~~~~~~~~")
+    print(f"round {el}")
+    print("~~~~~~~~~~~~~~~~~~")
+    foo.set(int(el))
+    bar.set('bar{}'.format(el))
+    print("Stored value for foo: {}".format(foo.get()))
+    print("Stored value for bar: {}".format(bar.get()))
+    time.sleep(2)
